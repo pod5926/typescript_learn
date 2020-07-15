92482ccc93f968380d39188dc09c1f460b45fc63
pod5926 <ks.apple.5926@gmail.com>
2020-07-15 13:10:23 +0900

    7/15

--- type/index.js
+++ type/index.js
@@ -1,11 +1,86 @@
-var hasvalue = false;
-// number は負数、少数も使える
+var hasValue = true;
 var count = 10;
-var float = 1.2;
-var box = {
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = 'hello';
+var back = "hello";
+var hello;
+hello = 'hello';
+var person = {
     name: {
-        first: 'aaa',
-        last: 'bbb'
-    }
+        first: 'Jack',
+        last: 'Smith'
+    },
+    age: 21
 };
-console.log(box.name.first);
+var fruits = ['Apple', 'Banana', 'Grape'];
+// 18-tuple
+var book = ['business', 1500, false];
+// 19-enum
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize["TALL"] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDE"] = 1] = "GRANDE";
+    CoffeeSize[CoffeeSize["VENTI"] = 2] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+};
+coffee.size = CoffeeSize.SHORT;
+// 20-any
+var anything = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+anything.fjiafjaj = 'faijfi';
+var banana = 'banana';
+banana = anything;
+// 21-union
+var unionType = 10;
+var unionTypes = [21, 'hello'];
+var apple = 'apple';
+var clothSize = 'large';
+var cloth = {
+    color: 'white',
+    size: 'medium'
+};
+// 24-function
+function add(num1, num2) {
+    return num1 + num2;
+}
+// 25-void
+function sayHello() {
+    // console.log('Hello!');
+}
+// console.log(sayHello());
+var tmp;
+// 27
+var anotherAdd = function (num1, num2) {
+    return num1 + num2;
+};
+var doubleNumber = function (num) { return num * 2; };
+// 28-coallback
+function doubleAndHandle(num, cb) {
+    var doubleNum = cb(num * 2);
+    console.log(doubleNum);
+}
+doubleAndHandle(21, function (doubleNum) {
+    return doubleNum;
+});
+var unknownInput;
+var anyInput;
+var text;
+unknownInput = 'hello';
+unknownInput = 21;
+unknownInput = true;
+text = anyInput;
+if (typeof unknownInput === 'string') {
+    text = unknownInput;
+}
+// function error(message: string) {
+//   throw new Error(message)
+// }
+// console.log(error('This is an error'))
--- type/index.ts
+++ type/index.ts
@@ -1,13 +1,104 @@
-let hasvalue: boolean = false
-// number は負数、少数も使える
-let count: number = 10
-let float: number = 1.2
+let hasValue = true
+let count = 10
+let float = 3.14
+let negative = -0.12
+let single = 'hello'
+let double = 'hello'
+let back = `hello`
+let hello: string
+hello = 'hello'
 
-const box = {
+const person = {
   name: {
-    first: 'aaa',
-    last: 'bbb',
+    first: 'Jack',
+    last: 'Smith',
   },
+  age: 21,
+}
+const fruits = ['Apple', 'Banana', 'Grape']
+
+// 18-tuple
+const book: [string, number, boolean] = ['business', 1500, false]
+
+// 19-enum
+enum CoffeeSize {
+  SHORT,
+  TALL = 'TALL',
+  GRANDE = 1,
+  VENTI,
 }
 
-console.log(box.name.first)
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL,
+}
+coffee.size = CoffeeSize.SHORT
+
+// 20-any
+let anything: any = true
+anything = 'hello'
+anything = ['hello', 33, true]
+anything = {}
+anything.fjiafjaj = 'faijfi'
+let banana = 'banana'
+banana = anything
+
+// 21-union
+let unionType: number | string = 10
+let unionTypes: (number | string)[] = [21, 'hello']
+
+// 22-literal & 23-type
+type ClothSize = 'small' | 'medium' | 'large'
+const apple = 'apple'
+let clothSize: ClothSize = 'large'
+const cloth: {
+  color: string
+  size: ClothSize
+} = {
+  color: 'white',
+  size: 'medium',
+}
+
+// 24-function
+function add(num1: number, num2: number): number {
+  return num1 + num2
+}
+
+// 25-void
+function sayHello(): void {
+  // console.log('Hello!');
+}
+
+// console.log(sayHello());
+let tmp: undefined
+
+// 27
+const anotherAdd: (n1: number, n2: number) => number = function (num1, num2) {
+  return num1 + num2
+}
+const doubleNumber: (num: number) => number = num => num * 2
+
+// 28-coallback
+function doubleAndHandle(num: number, cb: (num: number) => void): void {
+  const doubleNum = cb(num * 2)
+  console.log(doubleNum)
+}
+doubleAndHandle(21, doubleNum => {
+  return doubleNum
+})
+
+let unknownInput: unknown
+let anyInput: any
+let text: string
+unknownInput = 'hello'
+unknownInput = 21
+unknownInput = true
+text = anyInput
+if (typeof unknownInput === 'string') {
+  text = unknownInput
+}
+
+// function error(message: string) {
+//   throw new Error(message)
+// }
+// console.log(error('This is an error'))
